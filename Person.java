fd17720e1c155368169917b5795342a685248ec0
LuLu77777 <101054941+LuLu77777@users.noreply.github.com>
2022-05-08 22:44:24 +0800

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,29 @@
+//人
+ //眼睛：2
+ //身高：
+ //体重：
+ //年龄：
+ //国籍：
+ 
+ //吃饭：打饭，坐下，吃
+ //睡觉：上床，盖被子，睡
+ //学习：坐下，打开书本，学
+ 
+ class Person {
+	 int eyes = 2;
+	 double hight;
+	 double weight;
+	 int age;
+	 String nation;
+	 void eat() {
+		 System.out.print("打饭，坐下，吃");
+	 }
+	 void sleep() {
+		 System.out.print("上床，盖被子，睡");
+	 }
+	 void study() {
+		 System.out.print("坐下，打开书本，学");
+	 }
+ }
+ 
+ 
